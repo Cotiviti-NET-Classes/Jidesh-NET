77387ecc14f57db1e8620c8672dd7c8335a36767
g0dgamerz <jideshvaidya@gmail.com>
2022-04-18 09:07:46 +0545

    changed on main

--- day2/Program.cs
+++ day2/Program.cs
@@ -1,8 +1,35 @@
 ﻿using LearningBasic;
 using Task;
 
-Console.Write("hello world");
 
-int[] arr = {1,2,3};
+
+Console.WriteLine("enter your name");
+string name = Console.ReadLine();
+Console.WriteLine("hello {0}",name);
+Console.WriteLine("enter number of number from which you want find sum and average");
+int n=Convert.ToInt32(Console.ReadLine());
+int[] arr=new int[n];
+for(int i=0;i<n;i++)
+{
+  Console.Write("enter element number {0} : ",i);
+  arr[i] = Convert.ToInt32(Console.ReadLine()); 
+}
 Assignment a=new Assignment();
 Console.Write(a.sumandavg(arr));
+
+// Console.WriteLine("Hello press 1 for calculating sum and average using array");
+// Console.WriteLine("Hello press 2 for calculating sum and average using multiple params");
+// int a= Convert.ToInt32(Console.ReadLine());
+// switch(a)
+// {
+//     case 1:
+
+
+//     break;
+//     case 2:
+
+//     break;
+//     default:
+//     Console.Write("1 or 2 is only acceptable");
+//     break;
+// }
--- day2/assignement.cs
+++ day2/assignement.cs
@@ -3,7 +3,9 @@ namespace Task;
 class Assignment
 {
 
-    public string sumandavg(int []arr)
+
+//takes array as parameter and returns sum and average
+    public string sumandavg(int[] arr)
     {
         int add=0;
         float avg;
@@ -11,8 +13,24 @@ class Assignment
         {
             add +=arr[i];
         }
+        Console.Write(arr.Length);
         avg=add/arr.Length;
-        return $"the sum of array is {add} and average is {avg}";
+        return $"the sum of array = {add} and average = {avg}";
         
     }
+
+    //takes multiple parameter and returns sum and average
+    // public (int sum,float avg) sumavgmultipara(params int[] datas)
+    // {
+    //     int sum=0;
+    //     float avg;
+    //     foreach(int data in datas)
+    //     {
+    //         sum+=data;
+    //     }
+    //     avg=sum/datas.Length;
+    //     return (sum,avg);
+
+    // }
+
 }
